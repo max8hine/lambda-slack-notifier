02beb5a397a074622d2042936d1883504261984b
maxshine <max8hine@gmail.com>
2019-05-26 22:38:08 +1200

    fix error handling fashion

--- handler.js
+++ handler.js
@@ -1,40 +1,37 @@
 "use strict";
 const axios = require("axios");
 const createError = require("./helpers/error");
-const notifierError = createError("Slack Notifier");
+// const notifierError = createError("Slack Notifier");
 
-const responseDefault = {
+const resSuccessDefault = {
   statusCode: 200,
   body: JSON.stringify({ message: "Success!" }, null, 2)
 };
+
+const resErrorDefault = error => ({
+  statusCode: 501,
+  body: JSON.stringify(error, Object.getOwnPropertyNames(error), 2)
+});
 
 module.exports.notifier = async event => {
   try {
     const { notifierUrl } = process.env;
     await axios.post(notifierUrl, { text: event.body });
-    return responseDefault;
+    return resSuccessDefault;
   } catch (error) {
-    if (error.response) {
-      const { status, statusText } = error.response;
-      throw notifierError(`${error.status} ${error.statusText}`);
-    }
-    throw notifierError(error.message && error.message);
+    return resErrorDefault(error);
   }
 };
 
 module.exports.test = async event => {
   try {
-    const err = new Error("Oops!");
+    const err = new Error("*** function went wrong");
     await axios.post(
-      "http://localhost:8000/error",
-      JSON.stringify(err, null, 4)
+      "http://localhost:8000/notifier",
+      JSON.stringify(err, Object.getOwnPropertyNames(err), 2)
     );
-    return responseDefault;
+    return resSuccessDefault;
   } catch (error) {
-    if (error.response) {
-      const { status, statusText } = error.response;
-      throw notifierError(`${error.status} ${error.statusText}`);
-    }
-    throw notifierError(error.message && error.message);
+    return resErrorDefault(error);
   }
 };
